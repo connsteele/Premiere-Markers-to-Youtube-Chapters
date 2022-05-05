4b1f0f15e1c0e98716e33f710ac2dbd899ecc344
connsteele <35544211+connsteele@users.noreply.github.com>
2022-05-05 12:06:18 -0700

    Update MarkerClean.py
    Remove code that wasn't needed

--- MarkerClean.py
+++ MarkerClean.py
@@ -18,27 +18,26 @@ out = open(filepath + '\\' + 'CleanMarks.txt', 'w+')
 count = 0
 out.write("Video Chapters\n")
 
-with f:
-    for line in f:
-        # Skip the first line
-        if (count > 0):
-            # Get rid of all whitespace (including tabs)
-            strbuf = line.split()
-            chari = 0
-            for char in strbuf[1]:
-                if (chari > 2 and chari < 8):
-                    out.write(char)
-                chari += 1
-            out.write(' ')
-            # Write all remaining characters in the line and seperate words
-            for i in range (2, len(strbuf)):
-                for char in strbuf[i]:
-                    out.write(char)
-                # Add spaces between words, but not at the end of it all
-                if i < len(strbuf) - 1:
-                    out.write(' ')
-            out.write('\n')
-        count += 1        
+for line in f:
+    # Skip the first line
+    if (count > 0):
+        # Get rid of all whitespace (including tabs)
+        strbuf = line.split()
+        chari = 0
+        for char in strbuf[1]:
+            if (chari > 2 and chari < 8):
+                out.write(char)
+            chari += 1
+        out.write(' ')
+        # Write all remaining characters in the line and seperate words
+        for i in range (2, len(strbuf)):
+            for char in strbuf[i]:
+                out.write(char)
+            # Add spaces between words, but not at the end of it all
+            if i < len(strbuf) - 1:
+                out.write(' ')
+        out.write('\n')
+    count += 1        
 
 f.close()
 out.close()
